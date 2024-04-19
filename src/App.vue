8d3e91ffe8624961c7b73e0bdd11dd6de1303e5a
kimleang <leangk49@gmail.com>
2024-04-19 18:52:38 +0700

    Component event (parent listening to event from child component)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <script setup>
-
-
-import StaticPropsComponent from "./components/StaticPropsComponent.vue";
-import DynamicPropsComponent from "./components/DynamicPropsComponent.vue";
-import PropsValidation from "./components/PropsValidation.vue";
-import ComplexProps from "./components/ComplexProps.vue";
-import CustomValidator from "./components/CustomValidator.vue";
 import { ref } from "vue";
-const firstName =ref( 'leang')
-const lastName = ref('Kim')
-const myFavNum = ref(2)
+import ComponentEventVue from "./components/ComponentEvent.vue";
+let count = ref(0)
+const formHandler=(username,email,password)=>{
+console.log(username)
+console.log(email)
+console.log(password)
+}
 </script>
 
 <template>
-
- <StaticPropsComponent name="Jordan"/>
- <DynamicPropsComponent :firstName="firstName" :lastName="lastName"/>
-
- <PropsValidation fullName="Keo KimLeang" :age="20"/>
- <ComplexProps :friends="['Jack','Oggy','Leang']" :userInfo="{name:'Jack',age:20,location:['PP','Kd']}"/>
- <CustomValidator name="KimLeang" :age="20" password="123"/>
-
+  <h1>Count: {{count}}</h1>
+<ComponentEventVue @incrementCounter="count++" @userInfo="formHandler"/>
 </template>
 <!-- css -->
 <style scoped>
---
+++ src/components/ComponentEvent.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "vue";
+
+const username = ref('')
+const email = ref('')
+const passowrd = ref('')
+</script>
+<template>
+    <br>
+<button @click="$emit('incrementCounter')">+</button>
+<br>
+<h2>Form header</h2>
+<br>
+<form @submit.prevent="$emit('userInfo',username,email,password)">
+    <input type="text" v-model="username">
+    <input type="email" v-model="email">
+    <input type="password" v-model="password">
+    <button type="submit">Submit</button>
+</form>
+</template>
