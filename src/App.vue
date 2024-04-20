346f4c7ca379b6676f881ba384a6685622561654
leang <leangk49@gmail.com>
2024-04-20 19:18:47 +0700

    Lifecycle Hooks

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,12 @@
 <script setup>
-
-import { provide } from 'vue';
-import SchoolComponent from './components/SchoolComponent.vue';
-import PracticeComponent from './components/PracticeComponent.vue';
-provide('studentName','Leang')
-provide('studentAge',20)
-provide('studentLocations', ['PP',"KD"])
-
-//provide Array
-provide('friends',['jack','Oggy','Leang'])
-//provide objects 
-provide('game',{
-  id:1,
-  title:'Fortnite',
-  genre:['Action','FPS'],
-  developer:{
-    name:'leang',
-    location:['PP','KPS']
-  }
-})
-//provide array of object 
-provide('listOfGames',[
-  {
-    id:1,
-  title:'Fortnite',
-  genre:['Action','FPS'],
-  developer:{
-    name:'leang',
-    location:['PP','KPS']
-  }
-  },
-  {
-    id:2,
-  title:'Pubg',
-  genre:['Action','FPS'],
-  developer:{
-    name:'leang',
-    location:['PP','KPS']
-  }
-  },
-  {
-    id:3,
-  title:'valorant',
-  genre:['Action','FPS'],
-  developer:{
-    name:'leang',
-    location:['PP','KPS']
-  }
-  },
- 
-])
+import { ref } from 'vue';
+import LifeCycleComponent from './components/LifeCycleComponent.vue'
+const show = ref(true)
 </script>
 
 <template>
-  <SchoolComponent/>
-  <PracticeComponent/>
+ <LifeCycleComponent v-if="show"/>
+ <button @click="show= !show">Toggle Visible</button>
 </template>
 <!-- css -->
 <style scoped>
---
+++ src/components/LifeCycleComponent.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref,onBeforeMount,onMounted, onBeforeUpdate,onUpdated,onBeforeUnmount,onUnmounted} from "vue";
+const message = ref('Hello')
+const updateMesssage = ()=>{
+    message.value= 'Updated Hello'
+}
+//life cycle hooks
+onBeforeMount(()=>{
+    console.log('Component is about to be mounted')
+})
+onMounted(()=>{
+    console.log('Component has been mounted')
+})
+onBeforeUpdate(()=>{
+    console.log('Component is about to be updated')
+})
+onUpdated(()=>{
+    console.log('Component has been updated successfully')
+})
+onBeforeUnmount(()=>{
+    console.log('Component is about to be unmounted')
+})
+onUnmounted(()=>{
+    console.log('Component has been unmounted')
+})
+</script>
+<template>
+<p>{{ message }}</p>
+<button @click="updateMesssage">Update Message</button>
+</template>
